1f992fc5408b8bece8dfa4b7c2a944e456454375
thoelken <5861076+thoelken@users.noreply.github.com>
2024-09-12 22:03:37 +0200

    Adding hyperlinks to reference URLs and DOIs (#263)
    * added jekyll-scholar plugin and rule for non-public links
    
    * switch to natbib
    
    * frontiers
    
    * added markdown scholar plugin
    
    * This could work
    
    * chatgpt improved doi version
    
    * mfenner
    
    * paul sagt escape
    
    * escape
    
    * gpt ist schuld
    
    * gut
    
    * just match
    
    * escaped again
    
    * fix meister
    
    * ok
    
    * junge
    
    * ok test
    
    * talahon
    
    * test
    
    * test2
    
    * mazbe
    
    * incremental
    
    * self hosted
    
    * selfer hosted
    
    * added local build action
    
    * default build
    
    * added ruby setup
    
    * bundle
    
    * github only
    
    * remove local build (not working)

---
+++ _plugins/markdown.rb
@@ -0,0 +1,40 @@
+
+# See https://github.com/inukshuk/jekyll-scholar/issues/30
+require 'uri'
+
+# Updated URL_PATTERN to handle full DOI URLs and simple DOI patterns
+URL_PATTERN = Regexp.compile([
+  '\\\\href\\\\{([^\\\\}]+)\\\\}\\\\{([^\\\\}]+)\\\\}',  # For LaTeX href patterns
+  URI.regexp(['http', 'https']),  # For general URLs
+  # DOI patterns to match "10." followed by a series of characters,
+  # including full URLs like "https://doi.org/" or "http://dx.doi.org/"
+  '(https?:\/\/(doi\.org|dx\.doi\.org)\/10\.[0-9]{4,9}\/[-._;()\/:A-Z0-9]+)',
+  '(10\.[0-9]{4,9}\/[-._;()\/:A-Z0-9]+)'
+].join('|'), Regexp::IGNORECASE)
+
+module Jekyll
+  class Scholar
+    class Markdown < BibTeX::Filter
+      def apply(value)
+        value.to_s.gsub(URL_PATTERN) do |match|
+          if $1 # LaTeX href pattern
+            "[#{$2}](#{$1})"
+            "<a href=\"#{$1}\">#{$2}</a>"
+          elsif match =~ /https?:\/\/(doi\.org|dx\.doi\.org)\/(10\.[0-9]{4,9}\/[-._;()\/:A-Z0-9]+)$/i
+            # Handle full DOI URLs
+            doi_id = $2
+            "<a href=\"https://doi.org/#{doi_id}\">#{doi_id}</a>"
+          elsif match =~ /.*(10\.[0-9]{4,9}\/[-._;()\/:A-Z0-9]+)$/i
+            # Handle simple DOI patterns
+            doi = $&
+            "<a href=\"https://doi.org/#{doi}\">#{doi}</a>"
+          else
+            # Handle general URLs
+            #"test[#{match}](#{match})"
+            "<a href=\"#{match}\">#{match}</a>"
+          end
+        end
+      end
+    end
+  end
+end
